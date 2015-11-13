10b47fe8deb66e53f39830e619de56c4f2829591
Lénaïc Huard <lenaic@lhuard.fr>
2015-11-14 00:03:18 +0100

    Add a script to create systemd-nspawn Arch containers

---
+++ mkArchSystemdNspawnContainer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -euo pipefail
+
+readonly packages_to_skip=(
+    cryptsetup
+    device-mapper
+    dhcpcd
+    e2fsprogs
+    inetutils
+    jfsutils
+    licenses
+    linux
+    logrotate
+    lvm2
+    man-db
+    man-pages
+    mdadm
+    nano
+    netctl
+    pciutils
+    pcmciautils
+    perl
+    reiserfsprogs
+    s-nail
+    systemd-sysvcompat
+    texinfo
+    usbutils
+    xfsprogs
+)
+
+readonly packages_to_add=(
+    systemd
+)
+
+readonly base_packages=($(pacman -Sg base | cut -d' ' -f2))
+readonly packages_to_install="$(comm -23 <(IFS=$'\n'; sort <<<"${base_packages[*]}") <(IFS=$'\n'; sort <<<"${packages_to_skip[*]}")) ${packages_to_add[@]}"
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 image_name"
+    exit 1
+fi
+
+readonly root_dir="/var/lib/machines/$1"
+
+hash pacstrap || pacman -S --noconfirm arch-install-scripts
+btrfs subvolume create "$root_dir"
+pacstrap -c -d "$root_dir" $packages_to_install
+arch-chroot "$root_dir" systemctl enable systemd-networkd
+arch-chroot "$root_dir" systemctl enable systemd-resolved
+rm "$root_dir/etc/resolv.conf"
+ln -s /run/systemd/resolve/resolv.conf "$root_dir/etc/resolv.conf"
+cat >"$root_dir/etc/systemd/network/80-container-ipvlan.network" <<"EOF"
+[Match]
+Virtualization=container
+Name=iv-*
+
+[Network]
+DHCP=yes
+LinkLocalAddressing=yes
+
+[DHCP]
+UseTimezone=yes
+EOF
+
+# machinectl read-only "$1" true
