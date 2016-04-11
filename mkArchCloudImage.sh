d2121d839f85c090b50c76951f1723848375c940
Lénaïc Huard <lenaic@lhuard.fr>
2016-04-11 22:29:35 +0200

    Enable qemu-agent and ntp in Arch cloud image

--- mkArchCloudImage.sh
+++ mkArchCloudImage.sh
@@ -32,6 +32,7 @@ readonly packages_to_add=(
     openssh
     python2-requests
     cloud-init
+    qemu-guest-agent
     sudo
     vim
     dmidecode
@@ -41,12 +42,6 @@ function log {
     tput bold
     printf "$1\n"
     tput sgr0
-}
-
-function join {
-    local IFS="$1"
-    shift
-    echo "$*"
 }
 
 log "Loading nbd module..."
@@ -73,15 +68,13 @@ mkfs.btrfs -L "Arch Linux" -m single "$disk_device"
 mount "$disk_device" "$disk_mount"
 btrfs subvolume create "$disk_mount"/home
 btrfs subvolume create "$disk_mount"/var
-btrfs subvolume create "$disk_mount"/usr
 
 log "Installing the system..."
-# pacstrap "$disk_mount" base
 readonly base_packages=($(pacman -Sg base | cut -d' ' -f2))
 readonly packages_to_install="$(comm -23 <(IFS=$'\n'; sort <<<"${base_packages[*]}") <(IFS=$'\n'; sort <<<"${packages_to_skip[*]}")) ${packages_to_add[@]}"
-pacstrap "$disk_mount" $packages_to_install
+pacstrap -c "$disk_mount" $packages_to_install
 genfstab -U -p "$disk_mount" | sed 's|$disk_device|/dev/vda|g; /swap/d' >> "$disk_mount"/etc/fstab
-sed -i 's|^GRUB_CMDLINE_LINUX=".*|GRUB_CMDLINE_LINUX="init=/usr/lib/systemd/systemd"|;
+sed -i 's|^GRUB_CMDLINE_LINUX=".*|GRUB_CMDLINE_LINUX="init=/usr/lib/systemd/systemd console=ttyS0,115200n8 console=tty1"|;
         s|^GRUB_TIMEOUT=.*|GRUB_TIMEOUT=0|' "$disk_mount"/etc/default/grub
 arch-chroot "$disk_mount" grub-install --recheck --target=i386-pc "$disk_device"
 arch-chroot "$disk_mount" grub-mkconfig | sed "s|$disk_device|/dev/vda|g" > "$disk_mount"/boot/grub/grub.cfg
@@ -90,7 +83,8 @@ arch-chroot "$disk_mount" mkinitcpio -p linux
 arch-chroot "$disk_mount" systemctl enable systemd-networkd
 arch-chroot "$disk_mount" systemctl enable systemd-resolved
 rm "$disk_mount"/etc/resolv.conf
-ln -s /run/systemd/resolve/resolv.conf "$disk_mount"/etc/resolv.conf
+ln -s ../run/systemd/resolve/resolv.conf "$disk_mount"/etc/resolv.conf
+sed -i 's/\<dns\>/resolve/' "$disk_mount"/etc/nsswitch.conf
 cat >"$disk_mount"/etc/systemd/network/wired.network <<EOF
 [Match]
 Name=en*
@@ -98,6 +92,7 @@ Name=en*
 [Network]
 DHCP=ipv4
 EOF
+arch-chroot "$disk_mount" systemctl enable systemd-timesyncd.service
 arch-chroot "$disk_mount" systemctl enable sshd.socket
 sed -i 's/ - ubuntu-init-switch/#  - ubuntu-init-switch/;
         s/ - update_etc_hosts/#  - update_etc_hosts/;
@@ -112,6 +107,7 @@ arch-chroot "$disk_mount" systemctl enable cloud-init-local.service
 arch-chroot "$disk_mount" systemctl enable cloud-init.service
 arch-chroot "$disk_mount" systemctl enable cloud-config.service
 arch-chroot "$disk_mount" systemctl enable cloud-final.service
+arch-chroot "$disk_mount" systemctl enable qemu-ga.service
 
 umount -R "$disk_mount"
 qemu-nbd -d "$disk_device"
